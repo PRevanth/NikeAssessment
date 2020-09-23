2d8144c25b74e1e459a15eba1a43c3482df50492
Revanth <revanth.chowdary@kennametal.com>
2020-09-22 22:16:31 -0500

    Successfully added code to load Album details on WebView using WKWebKit.

--- NikeAssessment/ViewControllers/WebViewController.swift
+++ NikeAssessment/ViewControllers/WebViewController.swift
@@ -11,10 +11,53 @@ import WebKit
 class WebViewController: UIViewController {
     
     private var url: URL?
+    private var webView: WKWebView?
+    private let spinnerView = SpinnerView()
     
     convenience init(url: URL) {
         self.init()
         self.url = url
     }
     
+    override func loadView() {
+        setupWebView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadAlbum()
+    }
+    
+    private func setupWebView() {
+        spinnerView.setup(view: self.view)
+        spinnerView.startAnimating()
+        webView = WKWebView()
+        webView?.navigationDelegate = self
+        view = webView
+    }
+    
+    private func loadAlbum() {
+        guard let url = self.url else {
+            // Should handle error.
+            return
+        }
+        webView?.load(URLRequest(url: url))
+        webView?.allowsBackForwardNavigationGestures = true
+    }
+    
+}
+
+extension WebViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        spinnerView.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        spinnerView.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        // Should handle error if url failed to load webView
+        spinnerView.stopAnimating()
+    }
 }
